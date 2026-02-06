da72c3d93b52fc83647c9b790aacfaf031fc529e
Emerald love Hardee <elovehardee3@gmail.com>
2026-02-06 16:33:56 -0600

    Update ResultsScreen.tsx
    Update ResultsScreen.tsx

--- mobile/src/screens/ResultsScreen.tsx
+++ mobile/src/screens/ResultsScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+'import React  'react';
+import { View, Text, StyleSheet } ( 'react native';)
 
 /**
  * ResultsScreen
